a2f33203bcf2f8837adb7f29a159325cc8761f88
darwin <133578284+SpartakMezlumyan@users.noreply.github.com>
2024-05-06 19:19:53 +0400

    Add files via upload

---
+++ untitled folder/script.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  document.getElementById("demo").style.fontSize = "40px"; 
+  document.getElementById("demo").style.color = "white";
+  document.getElementById("demo").style.backgroundColor = "black";       
+}
